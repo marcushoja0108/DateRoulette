9dcd7d015b3cf50d7e76d38fcca8a7cf36390969
Stanislav588 <staskornienko01@gmail.com>
2024-10-18 09:37:06 +0200

    changes

--- JS/infoview.js
+++ JS/infoview.js
@@ -1,11 +1,29 @@
 updateInfoView();
-function updateInfoView(){
-    document.getElementById('app').innerHTML = `
-    <div class="header">
-        <div class="Box-left"></div>
+function updateInfoView() {
+  document.getElementById("app").innerHTML = `
+
+  <div class='header'>
+   <div class="Box-left"></div>
         <img class='header-img' src="th2.png"/>
         <div class="Box-right"></div>
+        </div>
     </div>
-        <h2 class="text">Infoview</h2>
+<div class='info-box'>
+        
+    <div class='date-img'>
+        <img src='./dateImage/cooking_class.jpg' />
+    </div>
+
+    <div class='info'>
+    <h1>Cooking date</h1>
+    <p>Info om date</p>
+    </div>
+
+        <div class='btn-container'>
+            <button>Tilbake</button>
+            <button>Aksepter</button>
+        </div>
+
+</div>
     `;
 }
